0e7e0d5bfc3ab2e8f4937ede98f867cf3c9aef6a
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-02-15 20:10:18 -0500

    can do outside companies now

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,11 +1,11 @@
 class Transaction < ApplicationRecord
-    validates :purchase_price, :purchase_shares, :average_price, :buy, :net_shares, presence: true
+    validates :purchase_price, :company_ticker, :purchase_shares, :average_price, :buy, :net_shares, presence: true
     validates :purchase_price, numericality: { greater_than: 0 }
     validates :net_shares, :average_price, numericality: { greater_than_or_equal_to: 0 }
 
     belongs_to :user
-    belongs_to :company,
-        primary_key: :ticker,
-        foreign_key: :company_ticker,
-        class_name: :Company
+    # belongs_to :company,
+    #     primary_key: :ticker,
+    #     foreign_key: :company_ticker,
+    #     class_name: :Company
 end
