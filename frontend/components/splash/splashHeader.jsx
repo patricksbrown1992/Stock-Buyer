72bf0becd92da6860fd0f233078ec4f8166333cc
patricksbrown1992 <patricksbrown1992@gmail.com>
2020-06-27 17:15:18 -0400

    links open up new tab

--- frontend/components/splash/splashHeader.jsx
+++ frontend/components/splash/splashHeader.jsx
@@ -9,9 +9,9 @@ const splashHeader = () => (
         </div>
         <div className="features-div">
             <ul>
-                <a href="https://patricksbrown1992.github.io/profile-site/">Portfolio</a>
-                <a href="https://www.linkedin.com/in/patricksbrown1992/">LinkedIn</a>
-                <a href="https://github.com/patricksbrown1992">Github</a>
+                <a href="https://patricksbrown1992.github.io/profile-site/" target="_blank">Portfolio</a>
+                <a href="https://www.linkedin.com/in/patricksbrown1992/" target="_blank">LinkedIn</a>
+                <a href="https://github.com/patricksbrown1992" target="_blank">Github</a>
 
             </ul>
         </div>
